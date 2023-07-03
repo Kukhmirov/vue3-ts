44b9b802715607b51d7e080f0586555236d0a594
Kukhmirov Vitaly <v.kukhmirov@just-work.org>
2023-07-03 14:22:22 +0300

    planet fix texture

--- src/assets/image/image.ts
+++ src/assets/image/image.ts
@@ -1,7 +1,9 @@
 import eartTexture from "@/assets/image/texture/2k_earth_daymap.jpg";
+import textureBump from "@/assets/image/texture/earthbump.jpg";
+import textureCloud from "@/assets/image/texture/earthCloud.png";
 import sunTexture from "@/assets/image/texture/lensflare0.png";
 import lensflare from "@/assets/image/texture/lensflare3.png";
-import milkSpace from "@/assets/image/texture/milch.jpg";
+import milkSpace from "@/assets/image/texture/milch.png";
 import dogSitter from "@/assets/image/work/dog-sitter.webp";
 import fond from "@/assets/image/work/fond.webp";
 import illusio from "@/assets/image/work/illusio.webp";
@@ -22,4 +24,6 @@ export {
     kids,
     notarial,
     shell,
+    textureBump,
+    textureCloud,
 };
